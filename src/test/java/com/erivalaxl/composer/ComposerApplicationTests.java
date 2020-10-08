25532f24ebef6cfe704301ab7436808b36db1a07
grimripper <someone@somewhere.com>
2020-10-08 10:09:26 -0700

    Initial commit

---
+++ src/test/java/com/erivalaxl/composer/ComposerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.erivalaxl.composer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComposerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
